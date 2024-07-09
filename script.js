95297af51dfe867672127cd2a5b68cf7cf7e57b5
emiliano <emilian.chiper.developer@gmail.com>
2024-07-09 18:53:18 +0300

    Create tile (i.e. cell) generation logic

--- script.js
+++ script.js
@@ -2,16 +2,42 @@
 
 // Import elements
 const gridElement = document.querySelector('.grid');
-const buttonElements = [...document.getElementsByTagName('button')];
+const inputField = document.getElementById('num');
+const buttonGen = document.querySelector('.generate');
+const tiles = [];
 
 // Create grid tiles
 const createTiles = function () {
-  const tile = document.createElement('div');
-  tile.classList.add('__tile');
-  tile.style.width = '100px';
-  tile.style.height = '100px';
-  tile.style.border = '1px solid black';
-  gridElement.append(tile);
+  const num = inputField.value;
+
+  // Get grid width
+  const gridWidth = gridElement.clientWidth;
+
+  // Get tile width
+  const tileWidth = gridWidth / num;
+  console.log(num, gridWidth, tileWidth);
+
+  // Generate and append tiles
+  Array.from({ length: num * num })
+    .map(() => {
+      const tile = document.createElement('div');
+      tile.classList.add('tile');
+
+      // Style tile element
+      tile.style.width = `${tileWidth}px`;
+      tile.style.height = `${tileWidth}px`;
+
+      return tile;
+    })
+    .forEach(tile => {
+      gridElement.append(tile);
+      tiles.push(tile);
+    });
+
+  console.log(tiles);
+
+  // Disable input field
+  inputField.disabled = true;
 };
 
-createTiles();
+buttonGen.addEventListener('click', createTiles);
