6eaf2992e29cba283d11643e379660445e85c41e
emiliano <emilian.chiper.developer@gmail.com>
2024-07-09 12:27:33 +0300

    Test creating and inserting one grid tile

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// Import elements
+const gridElement = document.querySelector('.grid');
+const buttonElements = [...document.getElementsByTagName('button')];
+
+// Create grid tiles
+const createTiles = function () {
+  const tile = document.createElement('div');
+  tile.classList.add('__tile');
+  tile.style.width = '100px';
+  tile.style.height = '100px';
+  tile.style.border = '1px solid black';
+  gridElement.append(tile);
+};
+
+createTiles();
